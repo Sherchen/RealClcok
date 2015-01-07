e7a653eb5a9ff413110cc40518cc3722e4868ce0
androidwsq <woaiandroid16@163.com>
2015-01-07 14:03:17 +0800

    initial version

---
+++ RealClock/src/com/dirtyhub/realclock/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dirtyhub.realclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.realclock);
+	}
+	
+
+}
